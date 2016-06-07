1dd102587b68ca497c8b6525572111bc65e0767c
George <george.cook.92@hotmail.co.uk>
2016-06-07 21:33:38 +0100

    adding css for controls - wired up controls component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
 
@@ -19,7 +20,16 @@ var Countdown = React.createClass({
         case 'started':
           this.startTimer();
           break;
-        default:
+
+        //intentionally no break here as if you stop, you also want the clear Interval
+        case 'stopped':
+          this.setState({count: 0});
+
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+
 
       }
     }
@@ -44,14 +54,29 @@ var Countdown = React.createClass({
     });
   },
 
+  handleStatusChange: function(newStatus) {
+    this.setState({
+      countdownStatus: newStatus
+    });
+  },
+
   render: function() {
     //after the prop gets called - it calls the handleSetCountdown function
 
-    var {count} = this.state;
+    var {count, countdownStatus} = this.state;
+
+    var renderControlArea = () => {
+      if (countdownStatus !== 'stopped') {
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange} />
+      } else {
+        return <CountdownForm onSetCountdown={this.handleSetCountdown} />
+      }
+    };
+
     return (
       <div>
         <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.handleSetCountdown} />
+        {renderControlArea()}
       </div>
     );
   }
